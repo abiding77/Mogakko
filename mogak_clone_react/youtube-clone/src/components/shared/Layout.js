23a42b80af64d8ab6478c06991f139cc79dfd2d3
Jung Ji Won <abiding77@naver.com>
2022-01-21 18:14:47 +0900

    mogak 3th

--- mogak_clone_react/youtube-clone/src/components/shared/Layout.js
+++ mogak_clone_react/youtube-clone/src/components/shared/Layout.js
@@ -1,17 +1,26 @@
 import styles from './Layout.module.css';
 import Header from './Header';
 import Menu from './Menu';
+import { useState } from 'react';
 
-function Layout({children}){
+function Layout({ children, activeMenu }) {
+
+    const [menuState, setMenu] = useState(true);
+    function controlMenu(){
+        setMenu(menuState=>!menuState);
+    }
     return (
-        <div className={styles.container}>
-            <Header/>
-            <div className={styles.layout}>
-                <Menu/>
-                    <div className={styles.contents}>{children}</div>
-            </div>
+      <div className={styles.container}>
+        <Header controlMenu={controlMenu}/>
+        <div className={styles.layout}>
+            {
+                menuState
+                ?<Menu activeMenu={activeMenu} /> :null
+            }
+          <div className={styles.contents}>{children}</div>
         </div>
-    )
-}
-
-export default Layout;
+      </div>
+    );
+  }
+  
+  export default Layout;
--- mogak_clone_react/youtube-clone/src/pages/Home.js
+++ mogak_clone_react/youtube-clone/src/pages/Home.js
@@ -2,8 +2,8 @@ import Layout from '../components/shared/Layout';
 
 function Home(){
     return(
-        <Layout>
-            <div>* 홈</div>
+        <Layout activeMenu="home">
+        <div>* 홈</div>
         </Layout>
     );
 }
