a16353d781c6968448ed42e492f27161c3364cab
Jung Ji Won <abiding77@naver.com>
2022-02-21 22:53:03 +0900

    번역사전 동적 크롤링

--- mogak_crawling/Day11.py
+++ mogak_crawling/Day11.py
@@ -4,6 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 
+
 chrome_driver = ChromeDriverManager().install()
 service = Service(chrome_driver)
 driver = webdriver.Chrome(service=service)
@@ -16,6 +17,7 @@ while(True):
 
     question = input("번역 할 영단어를 입력하세요. (0을 입력하면 종료됩니다.) : ")
     if question=='0':
+        print(words)
         break
     form = driver.find_element(By.CSS_SELECTOR, "textarea#txtSource")
     form.send_keys(question)
